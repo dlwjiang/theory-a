3c50f3b82ab3ad538fa4f6820a926287a763c86b
David Jiang <dlwjiang@gmail.com>
2018-12-03 09:57:13 -0800

    some styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,43 +2,47 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/Layout";
+import styled from 'styled-components';
+
+const Card = styled.div`
+  box-shadow: 1px 1px 1px 1px lightgrey;
+  border-radius: 5px;
+  padding: 2em 4em;
+`;
 
 export default class Index extends React.Component {
   render() {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
 
-    return (
-      <Layout>
+    return <Layout>
         <section className="section">
           <div className="container">
             {posts.map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: "1px solid #333", padding: "2em 4em" }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
+                <Card key={post.id}>
+                  <p>
+                    <Link
+                      className="has-text-primary"
+                      to={post.fields.slug}
+                    >
+                      {post.frontmatter.title}
+                    </Link>
+                    <span> &bull; </span>
+                    <small>{post.frontmatter.date}</small>
+                  </p>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link className="button is-small" to={post.fields.slug}>
+                      Keep Reading →
+                    </Link>
+                  </p>
+                </Card>
             ))}
           </div>
         </section>
-      </Layout>
-    );
+      </Layout>;
   }
 }
 
