717462495eac448ef06647419260440f15613119
David Jiang <dlwjiang@gmail.com>
2018-12-02 23:34:40 -0800

    cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,34 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item" title="Logo">
-          Theory A
-          <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/blog">
-          Blog
-        </Link>
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
+  <div>
+    <nav className="navbar is-transparent">
+      <div className="container">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/">
+            Home
+          </Link>
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          {/* <Link className="navbar-item" to="/products">
           Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
+        </Link> */}
+          {/* <Link className="navbar-item" to="/contact">
           Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
+        </Link> */}
+          {/* <Link className="navbar-item" to="/contact/examples">
           Form Examples
-        </Link>
-        <Link className="navbar-item" to="/scriptures">
-          Scriptures
-        </Link>
+        </Link> */}
+          <Link className="navbar-item" to="/scriptures">
+            Scriptures
+          </Link>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+    <div className="container"><b>Theory A</b></div>
+  </div>
 );
 
 export default Navbar
