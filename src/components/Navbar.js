d6c5bab3804b4b9caae342a1a3bb23ed7d6cc998
David Jiang <dlwjiang@gmail.com>
2018-12-02 23:48:36 -0800

    using styled-components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,39 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const Logo = styled.div`
+  color: red;
+  font-size: 30px;
+  margin-left: 5px;
+`;
 
 const Navbar = () => (
-  <div>
-    <nav className="navbar is-transparent">
-      <div className="container">
-        <div className="navbar-start">
-          <Link className="navbar-item" to="/">
-            Home
-          </Link>
-          <Link className="navbar-item" to="/about">
-            About
-          </Link>
-          {/* <Link className="navbar-item" to="/products">
-          Products
-        </Link> */}
-          {/* <Link className="navbar-item" to="/contact">
-          Contact
-        </Link> */}
-          {/* <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link> */}
-          <Link className="navbar-item" to="/scriptures">
-            Scriptures
-          </Link>
-        </div>
+  <nav className="navbar is-transparent">
+    <div className="container">
+      <div className="navbar-start">
+        <Logo>Theory A</Logo>
+        <Link className="navbar-item" to="/">
+          Home
+        </Link>
+        <Link className="navbar-item" to="/about">
+          About
+        </Link>
+        {/* <Link className="navbar-item" to="/products">
+        Products
+      </Link> */}
+        {/* <Link className="navbar-item" to="/contact">
+        Contact
+      </Link> */}
+        {/* <Link className="navbar-item" to="/contact/examples">
+        Form Examples
+      </Link> */}
+        <Link className="navbar-item" to="/scriptures">
+          Scriptures
+        </Link>
       </div>
-    </nav>
-    <div className="container"><b>Theory A</b></div>
-  </div>
+    </div>
+  </nav>
 );
 
 export default Navbar
